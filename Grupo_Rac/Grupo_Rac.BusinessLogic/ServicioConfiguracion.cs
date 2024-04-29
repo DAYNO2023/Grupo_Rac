7b5829e5125d7be04b0e5d02ea8b36a9719d0e29
Yordin Sanchez <146747575+DAYNO2023@users.noreply.github.com>
2024-04-29 11:24:34 -0600

    API
    :)

---
+++ Grupo_Rac/Grupo_Rac.BusinessLogic/ServicioConfiguracion.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+//using Grupo_Rac.BusinessLogic.Servicios;
+using Grupo_Rac.DataAccess;
+//using Grupo_Rac.DataAccess.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaRestaurante.BusinessLogic
+{
+    public static class ServicioConfiguracion
+    {
+        public static void DataAcces(this IServiceCollection service, string conn)
+        {
+
+            //Grupo_RacContext.BuildConnectionString(conn);
+
+        }
+        public static void BussinesLogic(this IServiceCollection service)
+        {
+            //service.AddScoped<ServiciosGenerales>();
+            //service.AddScoped<ServiciosAcceso>();
+            //service.AddScoped<ServicioRestaurantes>();
+        }
+    }
+}
