7d0a1987e886546ba1f9cb1672a2faddf9d8f0d2
Yordin Sanchez <146747575+DAYNO2023@users.noreply.github.com>
2024-05-01 05:31:18 -0600

    Api subida
    :)

---
+++ Grupo_Rac/Grupo_Rac.BusinessLogic/ServicioConfiguracion.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+//using Grupo_Rac.BusinessLogic.Servicios;
+using Grupo_Rac.DataAccess;
+//using Grupo_Rac.DataAccess.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grupo_Rac.DataAccess.Repositorio;
+using Grupo_Rac.BusinessLogic.Servicios;
+
+namespace Grupo_Rac.BusinessLogic
+{
+    public static class ServicioConfiguracion
+    {
+        public static void DataAcces(this IServiceCollection service, string conn)
+        {
+            service.AddScoped<DepartamentoRepositorio>();
+            GrupoRacContext.BuildConnectionString(conn);
+
+        }
+        public static void BussinesLogic(this IServiceCollection service)
+        {
+            service.AddScoped<GeneralService>();
+            //service.AddScoped<ServiciosAcceso>();
+            //service.AddScoped<ServicioRestaurantes>();
+        }
+    }
+}
---
+++ Grupo_Rac/Grupo_Rac.BusinessLogic/Servicios/GeneralService.cs
@@ -0,0 +1,120 @@
+﻿using GestionEmergencias.BussinesLogic;
+using Grupo_Rac.DataAccess.Repositorio;
+using Grupo_Rac.Entities.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo_Rac.BusinessLogic.Servicios
+{
+    public class GeneralService
+    {
+        private readonly DepartamentoRepositorio _departamentoRepositorio;
+        public GeneralService(DepartamentoRepositorio departamentoRepositorio)
+        {
+            _departamentoRepositorio = departamentoRepositorio;
+        }
+
+        #region Departamento
+
+        public ServiceResult ListDepto()
+        {
+            var result = new ServiceResult();
+            try
+            {
+                var lost = _departamentoRepositorio.List();
+                return result.Ok(lost);
+            }
+            catch (Exception ex)
+            {
+                return result.Error(ex.Message);
+            }
+        }
+        public ServiceResult InsertarDepto(tbDepartamento item)
+        {
+            var resul = new ServiceResult();
+            try
+            {
+                var lost = _departamentoRepositorio.Insertar(item);
+                if (lost.CodeStatus > 0)
+                {
+                    return resul.Ok(lost);
+                }
+                else
+                {
+                    lost.MessageStatus = (lost.CodeStatus == 0) ? "401 error de consulta" : lost.MessageStatus;
+                    return resul.Error(lost);
+                }
+            }
+            catch (Exception ex)
+            {
+                return resul.Error(ex.Message);
+            }
+        }
+
+
+
+        //public ServiceResult ActualizarDepto(tbDepartamento item)
+        //{
+        //    var resul = new ServiceResult();
+        //    try
+        //    {
+        //        var lost = _departamentoRepositorio.Update(item);
+        //        if (lost.CodeStatus > 0)
+        //        {
+        //            return resul.Ok(lost);
+        //        }
+        //        else
+        //        {
+        //            lost.MessageStatus = (lost.CodeStatus == 0) ? "401 error de consulta" : lost.MessageStatus;
+        //            return resul.Error(lost);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return resul.Error(ex.Message);
+        //    }
+        //}
+
+
+        //public ServiceResult EliminarDepto(string Dept_Id)
+        //{
+        //    var resul = new ServiceResult();
+        //    try
+        //    {
+        //        var lost = _departamentoRepositorio.DeleteS(Dept_Id);
+        //        if (lost.CodeStatus > 0)
+        //        {
+        //            return resul.Ok(lost);
+        //        }
+        //        else
+        //        {
+        //            return resul.Error(lost);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return resul.Error(ex.Message);
+        //    }
+        //}
+
+
+        //public ServiceResult DetallesDepto(string Dept_Id)
+        //{
+        //    var resul = new ServiceResult();
+        //    try
+        //    {
+        //        var lost = _departamentoRepositorio.findS(Dept_Id);
+        //        return resul.Ok(lost);
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return resul.Error(ex.Message);
+        //    }
+        //}
+        #endregion
+    }
+}
